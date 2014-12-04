adb0fc33b218b3a25a3b5bc0a0d867d6c3fbe28d
jonarnaldo <jonathanarnaldo@Jonathans-MacBook-Air.local>
2014-12-04 14:40:17 -0800

    adds route

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/foo', function(request, response) {
+  response.send('Hello World!');
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
