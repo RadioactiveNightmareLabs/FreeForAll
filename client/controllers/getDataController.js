e222ca624c6687b8d901a0bce16f680d78f3743d
jonarnaldo <jonathanarnaldo@Jonathans-MacBook-Air.local>
2014-12-08 12:38:12 -0800

    added walking dist

--- client/controllers/getDataController.js
+++ client/controllers/getDataController.js
@@ -1,42 +1,56 @@
+
+/*
+Order of operations
+
+populate the data with freeStuff/Places/Events
+  populate filter with objects
+    filter data by stuff/places/events
+      calculate distance from current position to each item
+        add filtered items to filteredItems
+          view is updated with filtered items
+*/
+
 angular.module('getFreeStuff.getDataController', [
   'getFreeStuff.services'
   ])
 
 .controller('getDataController', function ($scope, $http) {
 
-  $scope.freeStuff = [];
-  $scope.freePlaces = [{name: "foo", location: {lat: 45, lng: -122}}];
+  $scope.freeStuff = [{name: 'burgers', location: 'SOMA', type: 'foo'}];
+  $scope.freePlaces = [];
   $scope.freeEvents = [];
 
-
-  $scope.data = [];
-  $scope.currentLocation = {};
-
+  // used to filter the items
   $scope.filter = {};
   $scope.filteredItems = [];
-  $scope.address = {};
+
+  $scope.addressCoords = {};
+  $scope.addressString = {};
+
+  var distanceService = new google.maps.DistanceMatrixService();
+
 
   // finds distance between destination position and current location
-  // calculatDistance({lat: 46, lng: -122});
+  // calculatDistance({lat: 46.5, lng: -122.5});
   $scope.calculateDistance = function (positionObject) {
     // take out google maps, use data from incoming object
-    var destination = new google.maps.LatLng(positionObject.geometry.location.k, positionObject.geometry.location.B);
-    // ex
-    // {lat: 124, lng: 334}
+    var destination = new google.maps.LatLng(positionObject.lat, positionObject.lng);
 
     distanceService.getDistanceMatrix({
-      origins: [$scope.address],
+      origins: [$scope.addressCoords],
       destinations: [destination],
       travelMode: google.maps.TravelMode.WALKING,
     }, function(response, status) {
       var result = response.rows[0].elements[0];
-      
+
       var walkingDistance = result.distance.text;
       var walkingTime = parseInt(result.duration.text);
       
-      return ({walkingDistance: walkingDistance, walkingTime: walkingTime});
+      positionObject['walkingTime'] = walkingTime;
+      positionObject['walkingDistance'] = walkingDistance;
 
-      $scope.$apply();
+      console.log(positionObject);
+      $scope.$apply(); // not sure if this is needed
     });
   };
 
@@ -45,12 +59,30 @@ angular.module('getFreeStuff.getDataController', [
     // loop through places or events array
     arr.forEach(function (item) {
       // apply calculateDistance to each item
-      var distanceAndTime = $scope.calculateDistance(item.location);
+      $scope.calculateDistance(item.location);
         // add walking time and walking distance property to each item
-        item.distanceAndTime = distanceAndTime;
     });
   }
 
+  $scope.getLocation = function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      geocoder = new google.maps.Geocoder();
+      var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+        console.log(results);
+        if (status == google.maps.GeocoderStatus.OK) {
+          console.log('address acquired!');
+          // gets area of address
+          // this is liable to break because you can't guarantee same results...
+          $scope.addressString = results[3].address_components[0].long_name.toLowerCase();  
+          $scope.addressCoords = {lat: position.coords.latitude, lng: position.coords.longitude};
+          console.log($scope.addressString);
+          console.log($scope.addressCoords);
+        } 
+      });
+    });
+  }
 
   $scope.getFreePlaces = function () {
     return $http({
@@ -61,29 +93,10 @@ angular.module('getFreeStuff.getDataController', [
       angular.forEach(data, function (item) {
         $scope.freePlaces.push(item);
       })
+      console.log($scope.freePlaces);
     })
   }
-
-  $scope.getLocation = function () {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      geocoder = new google.maps.Geocoder();
-      var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-
-      geocoder.geocode({'latLng': latlng}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          // gets area of address
-          // this is liable to break because you can't guarantee same results...
-          $scope.address = results[3].address_components[0].long_name.toLowerCase();  
-          $scope.address = 'hayward';
-          console.log('address acquired!');
-          console.log($scope.address);
-
-          $scope.getFreeStuff();  
-        } 
-      });
-    });
-  }
-
+  
   $scope.getFreeStuff = function (string) {
     return $http({
       method: 'GET',
@@ -107,7 +120,6 @@ angular.module('getFreeStuff.getDataController', [
       var result = location.match(regex);
       return location.match(regex) ? result : false;
     }
-
 
     // loop through freeStuff
     angular.forEach($scope.freeStuff, function (item) {
@@ -135,5 +147,5 @@ angular.module('getFreeStuff.getDataController', [
   }
 
   // inialize on controller load
-  $scope.getLocation();
+  // $scope.getLocation();
 });
